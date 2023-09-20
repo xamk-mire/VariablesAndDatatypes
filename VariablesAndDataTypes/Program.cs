da709f70337eed23e97442b7a4ff50059180f9f7
Miika Reijonen <hmire02@xamk.fi>
2023-09-20 10:47:05 +0300

    Create solution

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1: Declare Variables
+            // Declare an int variable named age and initialize it with your age.
+            // Declare a double variable named height and initialize it with your height in meters (e.g., 1.75).
+            // Declare a char variable named initial and initialize it with the first letter of your name.
+            // Declare a bool variable named isStudent and initialize it with true or false based on your student status.
+            // Declare a string variable named name and initialize it with your name.
+            // Your code here
+
+
+            // Task 2: Display Variables
+            // Use Console.WriteLine() to display each variable along with a description (e.g., "My age is: [age]").
+            // Your code here
+
+
+            // Task 3: Type Conversion
+            // Convert the double height to an int. Store this in a new int variable named heightInCm.
+            // Convert the int age to a string. Store this in a new string variable named ageAsString.
+            // Your code here
+
+
+            // Task 4: Arithmetic Operations
+            // Calculate the sum of your age and heightInCm. Store the result in a new int variable named sum.
+            // Calculate the product of your age and height. Store the result in a new double variable named product.
+            // Your code here
+
+
+            // Task 5: Display Calculations
+            // Display the results of the calculations along with a description (e.g., "The sum of my age and height in cm is: [sum]").
+            // Your code here
+        }
+    }
+}
